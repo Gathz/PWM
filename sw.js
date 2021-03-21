f838c18aac0c3b4fab6a2e7bfc9cf8c5078b492c
FeVaDevs <feva.developers@gmail.com>
2021-03-21 19:46:59 +0100

    sw

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
 self.addEventListener("install", e => {
-    console.log("Install!");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./",
+                "./assets/frontend.min.css",
+                "./assets/js/frontend.min.js"
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
 });
